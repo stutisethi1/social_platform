41f8936a7db5571c7199209e29621e2043b87e6e
stutisethi1 <stutisethi075@gmail.com>
2026-04-24 00:16:22 +0530

    updated home UI with comments

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('like/<int:post_id>/', views.like_post, name='like_post'),
+    path('comment/<int:post_id>/', views.add_comment, name='add_comment'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 
 def like_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
@@ -8,5 +8,20 @@ def like_post(request, post_id):
         post.likes.remove(request.user)
     else:
         post.likes.add(request.user)
+
+    return redirect('home')
+
+
+def add_comment(request, post_id):
+    if request.method == "POST" and request.user.is_authenticated:
+        post = get_object_or_404(Post, id=post_id)
+        content = request.POST.get('content')
+
+        if content:
+            Comment.objects.create(
+                post=post,
+                author=request.user,
+                content=content
+            )
 
     return redirect('home')
